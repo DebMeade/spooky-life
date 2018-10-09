4997543448d7cf86df299e2c30364cf617dca7e1
jamiedehaan <jamie.price@comcast.net>
2018-10-09 13:38:00 -0600

    css life

--- spooky-life/client/src/pages/HauntedHouses/HauntedHouses.js
+++ spooky-life/client/src/pages/HauntedHouses/HauntedHouses.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+import Jumbotron from "../../components/Jumbotron";
 // import API from "../../utils/API";
 
-class Houses extends Component {
+class hauntedHouses extends Component {
   state = {
     houses: [],
     name: "",
@@ -11,42 +12,46 @@ class Houses extends Component {
     url: ""
   };
 
-  componentDidMount() {
-    this.loadHauntedHouses();
-  }
+  // componentDidMount() {
+  //   this.loadHauntedHouses();
+  // }
 
-  loadHauntedHouses = () => {
-    API.getHauntedHouses()
-    .then(res => 
-      this.setState({ houses: res.data, name: "", rating: "", location: "", phone: "", url: "" }))
-    .catch(err => console.log(err));
-  };
+  // loadHauntedHouses = () => {
+  //   API.getHauntedHouses()
+  //   .then(res => 
+  //     this.setState({ houses: res.data, name: "", rating: "", location: "", phone: "", url: "" }))
+  //   .catch(err => console.log(err));
+  // };
 
-  //below is if we want users to be able to add a hunted house
-  handleFormSubmit = event => {
-    event.preventDefault();
-    if (this.state.name && this.state.location && this.state.phone && this.state.url) {
-      API.saveHauntedHouse ({
-        name: this.state.name,
-        location: this.state.location,
-        phone: this.state.phone
-      })
-      .then(res => this.loadHauntedHouses())
-      .catch(err => console.log(err));
-    }
-  };
+  // //below is if we want users to be able to add a hunted house
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   if (this.state.name && this.state.location && this.state.phone && this.state.url) {
+  //     API.saveHauntedHouse ({
+  //       name: this.state.name,
+  //       location: this.state.location,
+  //       phone: this.state.phone
+  //     })
+  //     .then(res => this.loadHauntedHouses())
+  //     .catch(err => console.log(err));
+  //   }
+  // };
 
 
   //printing info to page
-  // render() {
-  //   return (
+  render() {
+    return (
+      // <Container>
+        <Jumbotron>
+          <h1>Haunted Houses</h1>
+        </Jumbotron>
+      // </Container>
 
-
-  //   );
-  // }
+    );
+  }
 
 
 
 }
 
-export default HauntedHouses;
+export default hauntedHouses;
