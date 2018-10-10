369bbac2349fd75403bf9109e4c80d3965ea6591
DebMeade <dkmeade@gmail.com>
2018-10-10 14:49:21 -0600

    lil scrape

--- spooky-life/routes/api/hauntedHouses.js
+++ spooky-life/routes/api/hauntedHouses.js
@@ -1,8 +1,54 @@
 const router = require("express").Router();
 const hauntedHousesController = require("../../controllers/hauntedHousesController");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const mongoose = require("mongoose");
+const db = require("../../models");
+
 
 // Matches with "/api/hauntedHoueses"
 router.route("/")
     .get(hauntedHousesController.findAll);
+
+router.route("/scrape")
+  .get(function(req, res) {
+  axios.get("http://hauntedhouses.com/haunted-houses/state/colorado/").then(function(response) {
+
+  var $ = cheerio.load(response.data);
+
+  $(".listing-details").each(function(i, element) {
+    var result = {};
+
+
+    result.name = $(this).children(".wpbdp-field-haunted_attraction_name").children(".value").children("a").text();
+    result.street = $(this).children(".wpbdp-field-street_address").children("span").text();
+    result.city = $(this).children(".wpbdp-field-city").children("span").text();
+    result.phone = $(this).children(".wpbdp-field-phone_number").children("span").text();
+    result.link = $(this).children(".wpbdp-field-haunted_attraction_name").children(".value").children().attr("href");
+    console.log(result);
+
+    db.hauntedHouses.create(result)
+    .then(function(dbhauntedHouse) {
+      console.log(dbhauntedHouse);
+    res.send("Scrape Complete");
+
+    })
+    .catch(function(err) {
+      return res.json(err);
+    });
+  });
+    
+  });
+});
+    
+    // app.get("/hauntedhouses", function(req, res) {
+    //   db.Article.find({})
+    //     .then(function(dbHauntedHouses) {
+    //       res.json(dbHauntedHouses);
+    //     })
+    //     .catch(function(err) {
+    //       res.json(err);
+    //     });
+    // });
 
 module.exports = router;
