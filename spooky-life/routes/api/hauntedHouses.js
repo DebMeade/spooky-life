37b91290b0802fbbef03995df6e3a61981d8a5a9
DebMeade <dkmeade@gmail.com>
2018-10-11 15:20:02 -0600

    only 1 set of haunted houses please

--- spooky-life/routes/api/hauntedHouses.js
+++ spooky-life/routes/api/hauntedHouses.js
@@ -8,47 +8,54 @@ const db = require("../../models");
 
 // Matches with "/api/hauntedHoueses"
 router.route("/")
-    .get(hauntedHousesController.findAll);
+  .get(hauntedHousesController.findAll);
 
 router.route("/scrape")
-  .get(function(req, res) {
-  axios.get("http://hauntedhouses.com/haunted-houses/state/colorado/").then(function(response) {
+  .get(function (req, res) {
+    axios.get("http://hauntedhouses.com/haunted-houses/state/colorado/").then(function (response) {
 
-  var $ = cheerio.load(response.data);
+      var $ = cheerio.load(response.data);
 
-  $(".listing-details").each(function(i, element) {
-    var result = {};
+      $(".listing-details").each(function (i, element) {
+        var result = {};
 
 
-    result.name = $(this).children(".wpbdp-field-haunted_attraction_name").children(".value").children("a").text();
-    result.street = $(this).children(".wpbdp-field-street_address").children("span").text();
-    result.city = $(this).children(".wpbdp-field-city").children("span").text();
-    result.phone = $(this).children(".wpbdp-field-phone_number").children("span").text();
-    result.link = $(this).children(".wpbdp-field-haunted_attraction_name").children(".value").children().attr("href");
-    console.log(result);
+        result.name = $(this).children(".wpbdp-field-haunted_attraction_name").children(".value").children("a").text();
+        result.street = $(this).children(".wpbdp-field-street_address").children("span").text();
+        result.city = $(this).children(".wpbdp-field-city").children("span").text();
+        result.phone = $(this).children(".wpbdp-field-phone_number").children("span").text();
+        result.link = $(this).children(".wpbdp-field-haunted_attraction_name").children(".value").children().attr("href");
+        console.log(result);
 
-    db.hauntedHouses.create(result)
-    .then(function(dbhauntedHouse) {
-      console.log(dbhauntedHouse);
-    res.send("Scrape Complete");
+        db.hauntedHouses.remove({})
+          .then(function (dropdb) {
+            db.hauntedHouses.create(result)
+              .then(function (dbhauntedHouse) {
+                console.log(dbhauntedHouse);
+                res.send("Scrape Complete");
 
-    })
-    .catch(function(err) {
-      return res.json(err);
+              })
+              .catch(function (err) {
+                return res.json(err);
+              });
+          })
+          .catch(function (err) {
+            return res.json(err);
+          });
+      })
+
+
     });
   });
-    
-  });
-});
-    
-    // app.get("/hauntedhouses", function(req, res) {
-    //   db.Article.find({})
-    //     .then(function(dbHauntedHouses) {
-    //       res.json(dbHauntedHouses);
-    //     })
-    //     .catch(function(err) {
-    //       res.json(err);
-    //     });
-    // });
+
+// app.get("/hauntedhouses", function(req, res) {
+//   db.Article.find({})
+//     .then(function(dbHauntedHouses) {
+//       res.json(dbHauntedHouses);
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+// });
 
 module.exports = router;
